65f84cb39ff866585430abd5d58851d1b83ef5b1
dawidfila <dawidfila00@gmail.com>
2025-10-21 23:23:04 +0200

    update

--- scripts/generate_logs.py
+++ scripts/generate_logs.py
@@ -1,6 +1,11 @@
 import random
 import datetime
 from faker import Faker
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
 
 fake = Faker()
 
@@ -8,14 +13,14 @@ def generate_log(n=50):
     logs = []
     event_types = ["login", "logout", "purchase", "error", "click", "view"]
     device = ["Mobile", "PC", "Tablet"]
-
+    
     for _ in range(n):
         log = {
             "timestamp": datetime.datetime.now().isoformat(),
-            "user_id": random.randint(1,1000),
+            "user_id": random.randint(1, 1000),
             "event_type": random.choice(event_types),
             "message": fake.sentence(nb_words=6),
-            "metdata": {
+            "metadata": {
                 "ip": fake.ipv4(),
                 "device": random.choice(device),
                 "country": fake.country()
@@ -24,8 +29,22 @@ def generate_log(n=50):
         logs.append(log)
     return logs
 
+def save_to_mongo(logs):
+    username = os.getenv("MONGO_USERNAME")
+    password = os.getenv("MONGO_PASSWORD")
+    database = os.getenv("MONGO_DATABASE")    
+    
+    client = MongoClient(
+        f"mongodb://{username}:{password}@localhost:27017/{database}",
+        authSource='admin'
+    )
+    
+    db = client.logs_db
+    raw_collection = db.raw_logs
+    raw_collection.insert_many(logs)
+    print(f"{len(logs)} logs saved in MongoDB")
+    client.close()
 
 if __name__ == "__main__":
     logs = generate_log(10)
-    for log in logs:
-        print(log)
+    save_to_mongo(logs)
